642c593980d917e7e142d4d4eb92cc79bf432bc2
David <75678655+David-Werth@users.noreply.github.com>
2024-11-20 20:20:46 +0400

    Add GET and SET commands

--- app/server.c
+++ app/server.c
@@ -1,13 +1,77 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/shm.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
+#define RESP_OK "+OK\r\n"
+
+typedef struct key_value {
+  char key[BUFFER_SIZE];
+  char value[BUFFER_SIZE];
+} key_value;
+
+static int shm_id;          // Make this global so signal handler can access it
+static key_value *kv_store; // Global pointer to shared memory
+
+// Signal handler for graceful shutdown
+void cleanup(int signum) {
+  printf("\nCleaning up shared memory...\n");
+
+  // Detach and free all key_value structures
+
+  // Detach from shared memory
+  if (shmdt(kv_store) == -1) {
+    printf("shmdt failed: %s\n", strerror(errno));
+  }
+
+  // Remove shared memory segment
+  if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
+    printf("shmctl failed: %s\n", strerror(errno));
+  }
+
+  exit(0);
+}
+
+void log_kv_store(key_value *kv_store, char *func_name) {
+  printf("Logging kv store in %s\n", func_name);
+  for (int i = 0; i < 100; i++) {
+    if (kv_store[i].key[0] != '\0') {
+      printf("Key: %s, Value: %s\n", kv_store[i].key, kv_store[i].value);
+    }
+  }
+}
+
+void add_to_kv_store(key_value *kv_store, char *key, char *value) {
+  for (int i = 0; i < 100; i++) {
+    if (kv_store[i].key[0] == '\0' || strcmp(kv_store[i].key, key) == 0) {
+      strncpy(kv_store[i].key, key, BUFFER_SIZE - 1);
+      kv_store[i].key[BUFFER_SIZE - 1] = '\0';
+
+      strncpy(kv_store[i].value, value, BUFFER_SIZE - 1);
+      kv_store[i].value[BUFFER_SIZE - 1] = '\0';
+
+      break;
+    }
+  }
+}
+
+char *get_from_kv_store(key_value *kv_store[], char *key) {
+  for (int i = 0; i < 100; i++) {
+    if (kv_store[i] != NULL && strcmp(kv_store[i]->key, key) == 0) {
+      printf("Found key: %s, value: %s\n", kv_store[i]->key,
+             kv_store[i]->value);
+      return kv_store[i]->value;
+    }
+  }
+  return NULL;
+}
 
 // Add helper functions for RESP parsing
 char *read_until_crlf(char *buffer, int *pos, int len) {
@@ -27,7 +91,6 @@ char *read_until_crlf(char *buffer, int *pos, int len) {
 
 void handle_client(int client_fd) {
   char buffer[BUFFER_SIZE];
-
   while (1) {
     int bytes_read = read(client_fd, buffer, sizeof(buffer));
     if (bytes_read <= 0)
@@ -44,14 +107,14 @@ void handle_client(int client_fd) {
 
         int num_elements = atoi(num_str);
         char *command = NULL;
-        char *echo_arg = NULL;
+        char *args[num_elements];
 
         // Read each bulk string in the array
         for (int i = 0; i < num_elements; i++) {
           if (buffer[pos] != '$') {
             break;
           }
-          pos++; // Skip $
+          pos++;
 
           char *len_str = read_until_crlf(buffer, &pos, bytes_read);
           if (!len_str) {
@@ -65,18 +128,31 @@ void handle_client(int client_fd) {
 
           if (i == 0) {
             command = strdup(element);
-          } else if (i == 1) {
-            echo_arg = strdup(element);
+          } else {
+            args[i] = strdup(element);
           }
         }
 
         if (command) {
           if (strcasecmp(command, "ping") == 0) {
             send(client_fd, "+PONG\r\n", 7, 0);
-          } else if (strcasecmp(command, "echo") == 0 && echo_arg) {
+          } else if (strcasecmp(command, "echo") == 0 && args[1]) {
             char response[BUFFER_SIZE];
             snprintf(response, BUFFER_SIZE, "$%d\r\n%s\r\n",
-                     (int)strlen(echo_arg), echo_arg);
+                     (int)strlen(args[1]), args[1]);
+            send(client_fd, response, strlen(response), 0);
+          } else if (strcasecmp(command, "set") == 0 && args[1] && args[2]) {
+            printf("Setting key: %s to value: %s\n", args[1], args[2]);
+            add_to_kv_store(kv_store, args[1], args[2]);
+            printf("KV Store after set: \n");
+            send(client_fd, RESP_OK, strlen(RESP_OK), 0);
+          } else if (strcasecmp(command, "get") == 0 && args[1]) {
+            printf("Getting key: %s\n", args[1]);
+            char *value = get_from_kv_store(&kv_store, args[1]);
+            printf("Value: %s\n", value);
+            char response[BUFFER_SIZE];
+            snprintf(response, BUFFER_SIZE, "$%d\r\n%s\r\n", (int)strlen(value),
+                     value);
             send(client_fd, response, strlen(response), 0);
           }
         }
@@ -88,12 +164,32 @@ void handle_client(int client_fd) {
 }
 
 int main() {
+  // Register signal handler for Ctrl+C
+  signal(SIGINT, cleanup);
+  signal(SIGTERM, cleanup);
+
   // Disable output buffering
   setbuf(stdout, NULL);
   setbuf(stderr, NULL);
 
   int server_fd, client_addr_len;
   struct sockaddr_in client_addr;
+
+  // Allocate shared memory for 100 key_value structures
+  shm_id = shmget(IPC_PRIVATE, sizeof(key_value) * 100, IPC_CREAT | 0666);
+  if (shm_id == -1) {
+    perror("shmget failed");
+    exit(EXIT_FAILURE);
+  }
+
+  kv_store = (key_value *)shmat(shm_id, NULL, 0);
+  if (kv_store == (key_value *)-1) {
+    perror("shmat failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Initialize the shared memory to zero
+  memset(kv_store, 0, sizeof(key_value) * 100);
 
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd == -1) {
